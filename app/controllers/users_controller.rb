8a0a67d6e0ba90bfd1dab342cde68c487c65062c
AyanoOkamuro <omochi185@gmail.com>
2021-08-02 09:26:34 +0000

    Finish bookers2_4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,23 @@
 class UsersController < ApplicationController
-  
+
   def index
     @users = User.all
     @book = Book.new
   end
-  
+
   def create
     book = Book.new(book_params)
     book.user_id = current_user.id
     book.save
     redirect_to book_path(book.id)
   end
-  
+
   def show
     @user = User.find(params[:id])
     @book = Book.new
     @books = @user.books
   end
-  
+
   def edit
     @user = User.find(params[:id])
     if @user != current_user
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
       #redirect_to user_path(current_user.id)
     #endだとなぜかダメ
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -39,15 +39,15 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-  
+
   private
 
   def book_params
     params.require(:book).permit(:title, :body, :user_id)
   end
-  
+
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
 end
